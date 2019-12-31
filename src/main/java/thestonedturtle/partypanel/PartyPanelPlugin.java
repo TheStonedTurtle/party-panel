4770fdc500db307a793689bfd74c968b67da0dbc
TheStonedTurtle <mgoodwin559@gmail.com>
2019-12-30 19:07:33 -0800

    More syncing

--- src/main/java/thestonedturtle/partypanel/PartyPanelPlugin.java
+++ src/main/java/thestonedturtle/partypanel/PartyPanelPlugin.java
@@ -10,15 +10,21 @@ import javax.inject.Inject;
 import javax.swing.SwingUtilities;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
 import net.runelite.client.account.AccountSession;
 import net.runelite.client.account.SessionManager;
+import net.runelite.client.chat.ChatColorType;
+import net.runelite.client.chat.ChatMessageBuilder;
+import net.runelite.client.chat.ChatMessageManager;
+import net.runelite.client.chat.QueuedMessage;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.PartyChanged;
+import net.runelite.client.game.ItemManager;
 import net.runelite.client.game.SpriteManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
@@ -27,6 +33,7 @@ import net.runelite.client.ui.NavigationButton;
 import net.runelite.client.util.ImageUtil;
 import net.runelite.client.ws.PartyService;
 import net.runelite.client.ws.WSClient;
+import net.runelite.http.api.ws.messages.party.UserJoin;
 import net.runelite.http.api.ws.messages.party.UserPart;
 import net.runelite.http.api.ws.messages.party.UserSync;
 import thestonedturtle.partypanel.data.PartyPlayer;
@@ -38,6 +45,9 @@ import thestonedturtle.partypanel.data.PartyPlayer;
 public class PartyPanelPlugin extends Plugin
 {
 	private final static BufferedImage ICON = ImageUtil.getResourceStreamFromClass(PartyPanelPlugin.class, "icon.png");
+
+	@Inject
+	private ChatMessageManager chatMessageManager;
 
 	@Inject
 	private Client client;
@@ -74,6 +84,10 @@ public class PartyPanelPlugin extends Plugin
 	@Getter
 	private PartyPlayer myPlayer = null;
 	private boolean fetchPlayerName = false;
+	private boolean doSync;
+
+	@Inject
+	ItemManager itemManager;
 
 	@Override
 	protected void startUp() throws Exception
@@ -97,15 +111,17 @@ public class PartyPanelPlugin extends Plugin
 			UUID uuid = accountSession != null ? accountSession.getUuid() : UUID.randomUUID();
 			wsClient.changeSession(uuid);
 		}
+
+		doSync = true;
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
 		clientToolbar.removeNavigation(navButton);
-		wsClient.unregisterMessage(PartyPlayer.class);
 		partyMembers.clear();
-		partyService.changeParty(null);
+		wsClient.unregisterMessage(PartyPlayer.class);
+		doSync = false;
 	}
 
 	boolean isInParty()
@@ -122,13 +138,65 @@ public class PartyPanelPlugin extends Plugin
 			return;
 		}
 
+		final boolean newMember = !partyMembers.containsKey(player.getMember().getMemberId());
 		partyMembers.put(player.getMember().getMemberId(), player);
+		if (newMember)
+		{
+			final ChatMessageBuilder builder = new ChatMessageBuilder()
+				.append(ChatColorType.HIGHLIGHT)
+				.append(player.getMember().getName());
+			if (player.getUsername() != null)
+			{
+				builder.append(" (")
+					.append(player.getUsername())
+					.append(")");
+			}
+			builder.append(" has joined the party!");
+
+			chatMessageManager.queue(QueuedMessage.builder()
+				.type(ChatMessageType.FRIENDSCHATNOTIFICATION)
+				.runeLiteFormattedMessage(builder.build())
+				.build());
+		}
+	}
+
+	@Subscribe
+	public void onUserJoin(final UserJoin event)
+	{
+		// TODO: Figure out how to support people not using the plugin
+		if (event.getMemberId().equals(partyService.getLocalMember().getMemberId()))
+		{
+			if (myPlayer == null)
+			{
+				myPlayer = new PartyPlayer(partyService.getLocalMember(), client, itemManager);
+			}
+			wsClient.send(myPlayer);
+		}
 	}
 
 	@Subscribe
 	public void onUserPart(final UserPart event)
 	{
-		partyMembers.remove(event.getMemberId());
+		final PartyPlayer removed = partyMembers.remove(event.getMemberId());
+
+		if (removed != null)
+		{
+			final ChatMessageBuilder builder = new ChatMessageBuilder()
+				.append(ChatColorType.HIGHLIGHT)
+				.append(removed.getMember().getName());
+			if (removed.getUsername() != null)
+			{
+				builder.append(" (")
+					.append(removed.getUsername())
+					.append(")");
+			}
+			builder.append(" has left the party!");
+
+			chatMessageManager.queue(QueuedMessage.builder()
+				.type(ChatMessageType.FRIENDSCHATNOTIFICATION)
+				.runeLiteFormattedMessage(builder.build())
+				.build());
+		}
 	}
 
 	@Subscribe
@@ -141,7 +209,7 @@ public class PartyPanelPlugin extends Plugin
 	public void onPartyChanged(final PartyChanged event)
 	{
 		partyMembers.clear();
-		myPlayer.setMember(partyService.getLocalMember());
+		myPlayer = null;
 	}
 
 	@Subscribe
@@ -156,6 +224,16 @@ public class PartyPanelPlugin extends Plugin
 		{
 			fetchPlayerName = true;
 		}
+
+		if (doSync && !partyService.getMembers().isEmpty())
+		{
+			// Request sync
+			final UserSync userSync = new UserSync();
+			userSync.setMemberId(partyService.getLocalMember().getMemberId());
+			wsClient.send(userSync);
+		}
+
+		doSync = false;
 	}
 
 	@Subscribe
@@ -171,7 +249,7 @@ public class PartyPanelPlugin extends Plugin
 			fetchPlayerName = false;
 			if (myPlayer == null || !myPlayer.getUsername().equals(client.getLocalPlayer().getName()))
 			{
-				myPlayer = new PartyPlayer(partyService.getLocalMember(), client);
+				myPlayer = new PartyPlayer(partyService.getLocalMember(), client, itemManager);
 				// member changed account, send new data to all members
 				wsClient.send(myPlayer);
 				SwingUtilities.invokeLater(panel::refreshUI);
