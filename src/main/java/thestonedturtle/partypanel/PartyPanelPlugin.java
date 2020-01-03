b9e852bf07e7f96416ea5e2c0f247566bb89a500
TheStonedTurtle <29030969+TheStonedTurtle@users.noreply.github.com>
2020-01-03 15:31:29 -0800

    Add party panel plugin (#1)
    adds a plugin that for use with the discord party feature

--- src/main/java/thestonedturtle/partypanel/PartyPanelPlugin.java
+++ src/main/java/thestonedturtle/partypanel/PartyPanelPlugin.java
@@ -1,16 +1,45 @@
 package thestonedturtle.partypanel;
 
 import com.google.inject.Provides;
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+import javax.annotation.Nullable;
 import javax.inject.Inject;
+import javax.swing.SwingUtilities;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
+import net.runelite.api.InventoryID;
+import net.runelite.api.Skill;
+import net.runelite.api.VarPlayer;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
+import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.api.events.StatChanged;
+import net.runelite.api.events.VarbitChanged;
+import net.runelite.client.account.AccountSession;
+import net.runelite.client.account.SessionManager;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.PartyChanged;
+import net.runelite.client.game.ItemManager;
+import net.runelite.client.game.SpriteManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.ClientToolbar;
+import net.runelite.client.ui.NavigationButton;
+import net.runelite.client.util.ImageUtil;
+import net.runelite.client.ws.PartyService;
+import net.runelite.client.ws.WSClient;
+import net.runelite.http.api.ws.messages.party.UserJoin;
+import net.runelite.http.api.ws.messages.party.UserPart;
+import net.runelite.http.api.ws.messages.party.UserSync;
+import thestonedturtle.partypanel.data.GameItem;
+import thestonedturtle.partypanel.data.PartyPlayer;
 
 @Slf4j
 @PluginDescriptor(
@@ -18,15 +47,271 @@ import net.runelite.client.plugins.PluginDescriptor;
 )
 public class PartyPanelPlugin extends Plugin
 {
+	private final static BufferedImage ICON = ImageUtil.getResourceStreamFromClass(PartyPanelPlugin.class, "icon.png");
+
 	@Inject
 	private Client client;
 
 	@Inject
+	private ClientToolbar clientToolbar;
+
+	@Inject
 	private PartyPanelConfig config;
+
+	@Inject
+	private PartyService partyService;
+
+	@Inject
+	private SessionManager sessionManager;
+
+	@Inject
+	SpriteManager spriteManager;
+
+	@Inject
+	ItemManager itemManager;
+
+	@Inject
+	private WSClient wsClient;
 
 	@Provides
 	PartyPanelConfig provideConfig(ConfigManager configManager)
 	{
 		return configManager.getConfig(PartyPanelConfig.class);
+	}
+
+	@Getter
+	private final Map<UUID, PartyPlayer> partyMembers = new HashMap<>();
+
+	private NavigationButton navButton;
+	private PartyPanel panel;
+	@Getter
+	private PartyPlayer myPlayer = null;
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		panel = new PartyPanel(this);
+		navButton = NavigationButton.builder()
+			.tooltip("Party Panel")
+			.icon(ICON)
+			.priority(7)
+			.panel(panel)
+			.build();
+
+		clientToolbar.addNavigation(navButton);
+		wsClient.registerMessage(PartyPlayer.class);
+
+		// If there isn't already a session open, open one
+		if (!wsClient.sessionExists())
+		{
+			AccountSession accountSession = sessionManager.getAccountSession();
+			// Use the existing account session, if it exists, otherwise generate a new session id
+			UUID uuid = accountSession != null ? accountSession.getUuid() : UUID.randomUUID();
+			wsClient.changeSession(uuid);
+		}
+
+		if (isInParty())
+		{
+			myPlayer = new PartyPlayer(partyService.getLocalMember(), client, itemManager);
+			wsClient.send(myPlayer);
+		}
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		clientToolbar.removeNavigation(navButton);
+		partyMembers.clear();
+		wsClient.unregisterMessage(PartyPlayer.class);
+	}
+
+	boolean isInParty()
+	{
+		// TODO: Determine if this is the correct way to check if we are in a party
+		return wsClient.sessionExists() && partyService.getLocalMember() != null;
+	}
+
+	@Subscribe
+	public void onPartyPlayer(final PartyPlayer player)
+	{
+		if (!isInParty())
+		{
+			return;
+		}
+
+		if (player.getMemberId().equals(partyService.getLocalMember().getMemberId()))
+		{
+			return;
+		}
+
+		player.setMember(partyService.getMemberById(player.getMemberId()));
+		if (player.getMember() == null)
+		{
+			return;
+		}
+
+		partyMembers.put(player.getMemberId(), player);
+		SwingUtilities.invokeLater(() -> panel.updatePartyPlayer(player));
+	}
+
+	@Subscribe
+	public void onUserJoin(final UserJoin event)
+	{
+		// TODO: Figure out how to support people not using the plugin
+		if (partyService.getLocalMember() == null)
+		{
+			return;
+		}
+
+		// Self joined
+		if (event.getMemberId().equals(partyService.getLocalMember().getMemberId()))
+		{
+			if (myPlayer == null)
+			{
+				myPlayer = new PartyPlayer(partyService.getLocalMember(), client, itemManager);
+			}
+			wsClient.send(myPlayer);
+		}
+	}
+
+	@Subscribe
+	public void onUserPart(final UserPart event)
+	{
+		final PartyPlayer removed = partyMembers.remove(event.getMemberId());
+		if (removed != null)
+		{
+			SwingUtilities.invokeLater(() -> panel.removePartyPlayer(removed));
+		}
+	}
+
+	@Subscribe
+	public void onUserSync(final UserSync event)
+	{
+		wsClient.send(myPlayer);
+	}
+
+	@Subscribe
+	public void onPartyChanged(final PartyChanged event)
+	{
+		partyMembers.clear();
+		SwingUtilities.invokeLater(panel::refreshUI);
+		myPlayer = null;
+	}
+
+	@Subscribe
+	public void onGameStateChanged(final GameStateChanged event)
+	{
+		if (!isInParty())
+		{
+			return;
+		}
+
+		if (event.getGameState().equals(GameState.LOGIN_SCREEN))
+		{
+			myPlayer = new PartyPlayer(partyService.getLocalMember(), client, itemManager);
+		}
+	}
+
+	@Subscribe
+	public void onGameTick(final GameTick tick)
+	{
+		if (!isInParty() || client.getLocalPlayer() == null)
+		{
+			return;
+		}
+
+		if (myPlayer == null)
+		{
+			myPlayer = new PartyPlayer(partyService.getLocalMember(), client, itemManager);
+			// member changed account, send new data to all members
+			wsClient.send(myPlayer);
+		}
+
+		if (myPlayer.getStats() == null)
+		{
+			myPlayer.updatePlayerInfo(client, itemManager);
+		}
+
+		if (!Objects.equals(client.getLocalPlayer().getName(), myPlayer.getUsername()))
+		{
+			myPlayer.setUsername(client.getLocalPlayer().getName());
+			wsClient.send(myPlayer);
+		}
+	}
+
+	@Subscribe
+	public void onStatChanged(final StatChanged event)
+	{
+		if (myPlayer == null || myPlayer.getStats() == null || !isInParty())
+		{
+			return;
+		}
+
+		final Skill s = event.getSkill();
+		if (myPlayer.getSkillBoostedLevel(s) == event.getBoostedLevel() && myPlayer.getSkillRealLevel(s) == event.getLevel())
+		{
+			return;
+		}
+
+		myPlayer.setSkillsBoostedLevel(event.getSkill(), event.getBoostedLevel());
+		myPlayer.setSkillsRealLevel(event.getSkill(), event.getLevel());
+		myPlayer.getStats().setTotalLevel(client.getTotalLevel());
+		wsClient.send(myPlayer);
+	}
+
+	@Subscribe
+	public void onItemContainerChanged(final ItemContainerChanged c)
+	{
+		if (!isInParty())
+		{
+			return;
+		}
+
+		if (c.getContainerId() == InventoryID.INVENTORY.getId())
+		{
+			myPlayer.setInventory(GameItem.convertItemsToGameItems(c.getItemContainer().getItems(), itemManager));
+		}
+		else if (c.getContainerId() == InventoryID.EQUIPMENT.getId())
+		{
+			myPlayer.setEquipment(GameItem.convertItemsToGameItems(c.getItemContainer().getItems(), itemManager));
+		}
+		else
+		{
+			return;
+		}
+
+		wsClient.send(myPlayer);
+	}
+
+	@Subscribe
+	public void onVarbitChanged(final VarbitChanged event)
+	{
+		if (myPlayer == null || myPlayer.getStats() == null || !isInParty())
+		{
+			return;
+		}
+
+		final int specialPercent = client.getVar(VarPlayer.SPECIAL_ATTACK_PERCENT) / 10;
+		if (specialPercent != myPlayer.getStats().getSpecialPercent())
+		{
+			myPlayer.getStats().setSpecialPercent(specialPercent);
+			wsClient.send(myPlayer);
+		}
+	}
+
+	@Nullable
+	PartyPlayer getPartyPlayerData(final UUID uuid)
+	{
+		if (!isInParty())
+		{
+			return null;
+		}
+
+		if (uuid.equals(myPlayer.getMemberId()))
+		{
+			return myPlayer;
+		}
+
+		return partyMembers.get(uuid);
 	}
 }
---
+++ src/main/java/thestonedturtle/partypanel/ui/PlayerPanel.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2020, TheStonedTurtle <https://github.com/TheStonedTurtle>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package thestonedturtle.partypanel.ui;
+
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import lombok.Getter;
+import net.runelite.api.EquipmentInventorySlot;
+import net.runelite.api.Skill;
+import net.runelite.api.SpriteID;
+import net.runelite.client.game.ItemManager;
+import net.runelite.client.game.SpriteManager;
+import net.runelite.client.ui.DynamicGridLayout;
+import net.runelite.client.ui.components.materialtabs.MaterialTab;
+import net.runelite.client.ui.components.materialtabs.MaterialTabGroup;
+import net.runelite.client.util.ImageUtil;
+import thestonedturtle.partypanel.data.GameItem;
+import thestonedturtle.partypanel.data.PartyPlayer;
+import thestonedturtle.partypanel.ui.equipment.EquipmentPanelSlot;
+import thestonedturtle.partypanel.ui.equipment.PlayerEquipmentPanel;
+import thestonedturtle.partypanel.ui.skills.PlayerSkillsPanel;
+import thestonedturtle.partypanel.ui.skills.SkillPanelSlot;
+
+@Getter
+public class PlayerPanel extends JPanel
+{
+	private static final Dimension IMAGE_SIZE = new Dimension(24, 24);
+
+	private PartyPlayer player;
+	private final SpriteManager spriteManager;
+	private final ItemManager itemManager;
+
+	private final PlayerBanner banner;
+	private final PlayerInventoryPanel inventoryPanel;
+	private final PlayerEquipmentPanel equipmentPanel;
+	private final PlayerSkillsPanel skillsPanel;
+
+	public PlayerPanel(final PartyPlayer selectedPlayer, final SpriteManager spriteManager, final ItemManager itemManager)
+	{
+		this.player = selectedPlayer;
+		this.spriteManager = spriteManager;
+		this.itemManager = itemManager;
+
+		this.banner = new PlayerBanner(selectedPlayer, spriteManager);
+		this.inventoryPanel = new PlayerInventoryPanel(selectedPlayer.getInventory(), itemManager);
+		this.equipmentPanel = new PlayerEquipmentPanel(selectedPlayer.getEquipment(), spriteManager, itemManager);
+		this.skillsPanel = new PlayerSkillsPanel(selectedPlayer, spriteManager, itemManager);
+
+		final JPanel view = new JPanel();
+		final MaterialTabGroup tabGroup = new MaterialTabGroup(view);
+		tabGroup.setBorder(new EmptyBorder(10, 0, 10, 0));
+
+		final MaterialTab inventory = new MaterialTab(createImageIcon(spriteManager.getSprite(SpriteID.TAB_INVENTORY, 0)), tabGroup, inventoryPanel);
+		inventory.setToolTipText("Inventory");
+		tabGroup.addTab(inventory);
+
+		final MaterialTab equipment = new MaterialTab(createImageIcon(spriteManager.getSprite(SpriteID.TAB_EQUIPMENT, 0)), tabGroup, equipmentPanel);
+		equipment.setToolTipText("Equipment");
+		tabGroup.addTab(equipment);
+
+		final MaterialTab skills = new MaterialTab(createImageIcon(spriteManager.getSprite(SpriteID.TAB_STATS, 0)), tabGroup, skillsPanel);
+		skills.setToolTipText("Skills");
+		tabGroup.addTab(skills);
+
+		setLayout(new DynamicGridLayout(0, 1));
+		add(banner);
+		add(tabGroup);
+		add(view);
+
+		tabGroup.select(inventory);
+
+		revalidate();
+		repaint();
+	}
+
+	private ImageIcon createImageIcon(BufferedImage image)
+	{
+		return new ImageIcon(ImageUtil.resizeImage(image, IMAGE_SIZE.width, IMAGE_SIZE.height));
+	}
+
+	// TODO add smarter ways to update data
+	public void changePlayer(final PartyPlayer newPlayer)
+	{
+		final boolean newUser = !newPlayer.getMemberId().equals(player.getMemberId());
+
+		player = newPlayer;
+		banner.setPlayer(player);
+		inventoryPanel.updateInventory(player.getInventory());
+
+		for (final EquipmentInventorySlot equipSlot : EquipmentInventorySlot.values())
+		{
+			final EquipmentPanelSlot slot = this.equipmentPanel.getPanelMap().get(equipSlot);
+			final GameItem item = player.getEquipment()[equipSlot.getSlotIdx()];
+			if (item != null)
+			{
+				slot.setGameItem(item, itemManager.getImage(item.getId(), item.getQty(), item.isStackable()));
+			}
+			else
+			{
+				slot.setGameItem(null, null);
+			}
+		}
+
+		if (newUser)
+		{
+			banner.recreatePanel();
+		}
+
+		if (player.getStats() != null)
+		{
+			banner.refreshStats();
+			for (final Skill s : Skill.values())
+			{
+				if (s.equals(Skill.OVERALL))
+				{
+					continue;
+				}
+
+				final SkillPanelSlot panel = skillsPanel.getPanelMap().get(s);
+				panel.updateBoostedLevel(player.getStats().getBoostedLevels().get(s));
+				panel.updateBaseLevel(player.getStats().getBaseLevels().get(s));
+			}
+			skillsPanel.getTotalLevelPanel().updateTotalLevel(player.getStats().getTotalLevel());
+		}
+	}
+}
